a6100e0d902496273c1074702eb93cc97c044929
Stephen <s.guild2@students.clark.edu>
2020-04-27 13:06:24 -0700

    Mod 2 Lab 1

---
+++ Mod 2 Lab 1.py
@@ -0,0 +1,58 @@
+"""
+CTEC 121
+<your name>
+<assignment/lab name>
+<assignment/lab description
+"""
+
+""" IPO template
+Input(s): list/description
+Process: description of what function does
+Output: return value and description
+"""
+
+def main():
+    """
+    A=2
+    print("\n", A, "\n")
+    a=22
+    print(a)
+    print()
+
+    AAA=2
+    print(AAA)
+    print()
+
+    Spam=222
+    print()
+    print(Spam)
+    print(Spam)
+    print()
+
+    #Exponents
+    print(2)
+    print(3-1)
+    print((3-1)/2)
+    print(5/3)
+    print(5//3)
+    print(5%3)
+    print(2+((3-1)/2))
+
+    print()
+    print("The answer is", 3+4, sep="-", end="\t")
+    print("End")
+    print()
+
+    for i in range(5)
+    print(i, end="\t")
+    print()
+    """
+    #exercise
+    ClassCode="CTEC 121"
+    ClassCount="18"
+    ClassName="Introduction to Programming and Problem Solving"
+    #Welcome to <ClassCode>, The student count is (ClassCount), and the name of the class is <ClassName>
+    print("Welcome to", ClassCode, "The student count is", ClassCount, "and the name of the class is", ClassName, ".", sep=" ")
+    
+
+main()    
